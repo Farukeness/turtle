e8cf34a0bc2223fb442c45758c44969109abe5b0
Faruk Enes Zan <farukenes10825@gmail.com>
2024-10-14 12:54:07 +0300

    random turtle and counter update

--- catch_the_turtle.py
+++ catch_the_turtle.py
@@ -3,24 +3,38 @@ import time
 import random
 sc = turtle.Screen()
 sc.bgcolor("light blue")
-
 catch_turtle = turtle.Turtle()
+count_turtle = turtle.Turtle()
 
 
+
+
+
+
+
+def counter():
+    count_turtle.hideturtle()
+    count_turtle.up()
+    height = sc.window_height()
+    y = height * 0.85 /2
+    count_turtle.goto(0,y)
+    count_turtle.write("Score:0",align="center",font=("Arial",20,"normal"))
 
 
 def random_turtle():
-    
-    catch_turtle.shape("turtle")
     catch_turtle.up()
+    catch_turtle.shape("turtle")
     x = random.randint(-300,300)
     y = random.randint(-300,300)
-    catch_turtle.goto(x,y)
+    catch_turtle.setposition(x,y)
     catch_turtle.stamp()
-    time.sleep(0.2)
     catch_turtle.clear()
+    time.sleep(1)
 
-
-random_turtle()
+while True:
+    turtle.tracer(0)
+    counter()
+    random_turtle()
+    turtle.tracer(1)
 
 turtle.mainloop()
